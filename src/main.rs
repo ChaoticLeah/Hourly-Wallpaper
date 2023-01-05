86d63cc084a6b65441be6cf541f58bfa363a74e3
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2023-01-05 21:10:02 +0000

    cleaned up comments

--- src/main.rs
+++ src/main.rs
@@ -6,49 +6,6 @@ mod api;
 
 
 
-// fn load_config() -> Result<yaml_rust::Yaml, Box<dyn Error>> {
-//     let config_file_string = fs::read_to_string("config.yaml")?;
-//     let docs = YamlLoader::load_from_str(config_file_string.as_str()).unwrap();
-//     // Get the first document
-//     let doc = &docs[0];
-//     // Debug support
-//     // println!("{:?}", doc);
-
-
-
-//     Ok(doc.to_owned())
-// }
-
-//TODO: Move all this query stuff to its own file
-// struct Query {
-//     query: String, 
-//     categories: String, 
-// }
-
-// fn get_random_query(config : &yaml_rust::Yaml) -> Query {
-//     if let Some(rand_query_data) = config["query_data"].as_vec().unwrap().choose(&mut rand::thread_rng()) {
-//         if let Some(map) = rand_query_data.as_hash() {
-//             let query = map.get(&yaml_rust::Yaml::String("query".to_string()));
-//             let categories = map.get(&yaml_rust::Yaml::String("categories".to_string()));
-
-//             //A query is always required so dont worry about checking that
-//             return Query {
-//                 query:  query.to_owned().unwrap().as_str().unwrap().to_string(), 
-//                 categories: if categories.to_owned().is_none() || categories.to_owned().unwrap().as_str().is_none() {
-//                     "".to_owned()
-//                 } else{
-//                     categories.to_owned().unwrap().as_str().unwrap().to_string()
-//                 },
-//             };
-//         }
-//     }
-    
-//     Query {
-//         query: "".to_string(),
-//         categories: "".to_string(),
-//     }
-// }
-
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
